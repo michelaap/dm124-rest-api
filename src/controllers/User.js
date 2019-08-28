d74f98e8241f2c0ca83489bed77ff4a74dcfc422
Michel Pereira <michel.augusto@pg.inatel.br>
2019-08-27 21:55:13 -0300

    user controller initial implements

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -5,7 +5,11 @@ module.exports = {
 
   },
   async getUser(request, response) {
-
+    const userId = request.params.id;
+    const user = await User
+      .findById(userId)
+      .select('_id name email role');
+    return response.json(user);
   },
   async getUsers(request, response) {
     const users = await User
@@ -15,9 +19,36 @@ module.exports = {
     return response.json(users);
   },
   async updateUser(request, response) {
+    const userId = request.params.id;
+    const { role } = request.body;
+    const user = await User.findById(userId);
 
+    if (!user) {
+      return response
+        .status(404)
+        .json({ message: 'User not found!' })
+    }
+
+    user.role = role;
+    await user.save();
+
+    const userUpdated = {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    }
+    return response.json(userUpdated);
   },
   async deleteUser(request, response) {
+    const userId = request.params.id;
+    const user = await User.findByIdAndRemove(userId);
 
+    if (!user) {
+      return response
+        .status(404)
+        .json({ message: 'User not found!' });
+    }
+    return response.json({ message: 'User deleted!' });
   }
 }
