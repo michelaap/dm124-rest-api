3c794a1c49b144ac5365dd7bb69f6117c7192f41
Michel Pereira <michel.augusto@pg.inatel.br>
2019-08-27 21:22:32 -0300

    add connection with mongo atlas

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const server = http
   .createServer(require('./app'));
 
-server.listen(process.env.PORT || 3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-xf5rx.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true }
+  ).then(() => {
+    server.listen(process.env.PORT || 3000);
+  });
