0cbfb0ca490c8b26258c12d4b4b912e3a646dbf3
Michel Pereira <michel.augusto@pg.inatel.br>
2019-08-28 21:05:46 -0300

    route order.post with auth

--- src/routes/order.js
+++ src/routes/order.js
@@ -2,11 +2,12 @@ const express = require('express');
 const routes = express.Router();
 
 const OrderController = require('../controllers/Order');
+const auth = require('../middleware/auth');
 
 routes.get('/', OrderController.getOrders);
 routes.get('/:id', OrderController.getOrder);
 
-routes.post('/', OrderController.createOrder);
+routes.post('/', auth, OrderController.createOrder);
 routes.patch('/', OrderController.updateOrder);
 routes.delete('/:id', OrderController.deleteOrder);
 
