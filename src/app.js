e6e7ac553a4bedf13e7339b770c2d01b1a843dce
Michel Pereira <michel.augusto@pg.inatel.br>
2019-08-30 21:20:30 -0300

    add libs for security

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
 const app = express();
 
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/users', require('./routes/user'));
